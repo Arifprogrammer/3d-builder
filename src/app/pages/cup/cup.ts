1b45404e53d6535925def26a2d589cc59a88eebc
Arifprogrammer <arifmgtcu@gmail.com>
2025-12-03 00:15:09 +0600

    implemented 3d cup

--- src/app/pages/cup/cup.ts
+++ src/app/pages/cup/cup.ts
@@ -1,10 +1,10 @@
-import { Component, ElementRef, viewChild, CUSTOM_ELEMENTS_SCHEMA, computed } from '@angular/core';
-import { extend, injectBeforeRender, injectLoader, NgtArgs } from 'angular-three';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, computed } from '@angular/core';
+import { extend, injectLoader, injectStore, NgtArgs } from 'angular-three';
 import * as THREE from 'three';
-import { Mesh } from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-extend(THREE);
+extend({ ...THREE, OrbitControls });
 
 @Component({
   imports: [NgtArgs],
@@ -13,22 +13,19 @@ extend(THREE);
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Cup {
-  meshRef = viewChild.required<ElementRef<Mesh>>('mesh');
+  ngtStore = injectStore();
   gltf = injectLoader(
     () => GLTFLoader,
-    () => 'assets/scene.gltf'
+    () => 'assets/cup/scene.gltf'
   );
 
   model = computed(() => {
     const gltf = this.gltf();
-    return gltf ? gltf.scene : null;
+    return gltf?.scene || null;
   });
 
-  constructor() {
-    /* injectBeforeRender(({ delta }) => {
-      const mesh = this.meshRef().nativeElement;
-      mesh.rotation.x += delta;
-      mesh.rotation.y += delta;
-    }); */
-  }
+  camera = this.ngtStore.select('camera');
+  glDom = this.ngtStore.select('gl', 'domElement');
+
+  constructor() {}
 }
